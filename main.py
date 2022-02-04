c090063b543ee7d12158885b979adbea50205530
magga-sola <margreta19@ru.is>
2022-02-04 11:44:08 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 class SleePy:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
 
-p1 = SleePy("John", 36)
+  """
 
-print(p1.name)
-print(p1.age) 
+  
+  
+  
+  
+  
+  
+  """
+
+  def __init__(self,  edf, display, segmentation, feuture ):
+
+    self.edf = edf
+    edf.segmentation = segmentation
+    edf.feuture = feuture
+    feuture.display = display
+
+
+  def file_extraction(file_name, file_type):
+      """ File Extraction with file name and file type as input """
+    #if file_type == valid:
+    #  open file_name
+      return 0
+  
+  def edf(edf_list):
+    """ this does this """
+    return 
+    
+
+def read_raw_edf(file_name):
+  """ this does this"""
+  return
